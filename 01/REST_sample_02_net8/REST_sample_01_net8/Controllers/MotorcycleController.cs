a47404e7ee66a3125f4b32358e46b0a6aa8c5660
Nagy Máté <nagy.mano.mate@gmail.com>
2025-09-06 15:17:53 +0200

    homework done

---
+++ 01/REST_sample_02_net8/REST_sample_01_net8/Controllers/MotorcycleController.cs
@@ -0,0 +1,56 @@
+﻿namespace REST_sample_02_net8.Controllers;
+
+public class PhoneController : ControllerBase
+{
+    public List<string> Phones = [
+        "IPhone 16 Pro Max",
+        "Samsung Galaxy S25 Ultra",
+        "Huawei Mate 20"
+    ];
+
+    [HttpGet]
+    [Route("phone/id/{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute][Required]int id){
+        return Ok(Phones[id]);
+    }
+
+    [HttpGet]
+    [Route("phone")]
+    public async Task<IActionResult> GetByQueryAsync([FromQuery][Required] int id)
+    {
+        return Ok(Phones[id]);
+    }
+
+    [HttpGet]
+    [Route("phone/all")]
+    public async Task<IActionResult> GetByAllAsync(){
+        return Ok(Phones);
+    }
+
+    [HttpPost]
+    [Route("phone/create")]
+    public async Task<IActionResult> CreateAsync([FromBody][Required]string phone)
+    {
+        Phones.Add(phone);
+
+        return Ok(Phones);
+    }
+
+    [HttpPut]
+    [Route("phone/update")]
+    public async Task<IActionResult> UpdateAsync([FromBody][Required] PhoneUpdateModel model)
+    {
+        Phones[model.Id] = model.Name;
+
+        return Ok(Phones);
+    }
+
+    [HttpDelete]
+    [Route("phone/delete/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute][Required] int id)
+    {
+        Phones.RemoveAt(id);
+
+        return Ok(Phones);
+    }
+}
---
+++ 01/REST_sample_02_net8/REST_sample_01_net8/Models/PhoneUpdateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace REST_sample_02_net8.Models;
+
+public class PhoneUpdateModel
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
